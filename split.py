52a7732c57a6494046dc0525ffd69dbc4afb63da
Rogener Castilho <rogener.castilho@gmail.com>
2018-07-08 10:38:13 -0300

    Changed from commmand line to GUI

--- split.py
+++ split.py
@@ -1,21 +1,19 @@
 import subprocess
 import sys
-
+import easygui
 
 def main():
     """split a music track into specified sub-tracks by calling ffmpeg from the shell"""
 
-    # check command line for original file and track list file
-    if len(sys.argv) != 3:
-        print('usage: split <original_track> <track_list>')
-        exit(1)
-
-    # record command line args
-    original_track = sys.argv[1]
-    track_list = sys.argv[2]
+    #Registering user input of files.
+    #First is to select audio file for splitting, second is the text file with time and tracks.
+    easygui.msgbox("Choose file to split")
+    original_track = easygui.fileopenbox()
+    easygui.msgbox("Choose text file that contains Start | End Times and titles")
+    track_list = easygui.fileopenbox()
 
     # create a template of the ffmpeg call in advance
-    cmd_string = 'ffmpeg -i {tr} -acodec copy -ss {st} -to {en} {nm}.opus'
+    cmd_string = 'ffmpeg -i {tr} -acodec copy -ss {st} -to {en} {nm}.m4a'
 
     # read each line of the track list and split into start, end, name
     with open(track_list, 'r') as f:
